d0d9d22d883dd37cb0830cb1bef2b77e9a909afb
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-07-28 03:17:08 +0600

    tesolve

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -30,9 +30,51 @@ async function run() {
 
 
 
+    const coffeeCollaction = client.db('coffeeDB').collection('coffees')
 
 
-    
+    app.get('/coffees', async(req, res) => {
+        const result = await coffeeCollaction.find().toArray()
+        res.send(result)
+    })
+ app.get('/coffees/:id', async(req,res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await coffeeCollaction.findOne(query)
+    res.send(result)
+
+ })
+
+  app.post('/coffees', async(req, res) => {
+    const newCoffee = req.body;
+    // console.log(newCoffee)
+    const result = await coffeeCollaction.insertOne(newCoffee)
+    res.send(result)
+  }) 
+
+  
+  //put
+  app.put('/coffees/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const options = { upsert: true };
+  const updateCoffee = req.body;
+
+  const updateDoc = {
+    $set: updateCoffee
+  };
+
+  const result = await coffeeCollaction.updateOne(filter, updateDoc, options);
+  res.send(result);
+});
+
+
+  app.delete('/coffees/:id', async(req,res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await coffeeCollaction.deleteOne(query)
+    res.send(result)
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
